d21939598dea0ee87b0c10bc663319c56cd5cd7b
tuan782000 <thaituan7820@gmail.com>
2024-05-20 23:48:10 +0700

    add iconsax and react native vector icon

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {View, Text, ScrollView} from 'react-native';
 import React from 'react';
 import {fonts} from './src/constants/fonts';
 import {globalStyles} from './src/styles/globalStyles';
+import {colors} from './src/constants/colors';
 
 type Props = {};
 
@@ -13,29 +14,139 @@ const App = (props: Props) => {
       <Text style={{fontFamily: 'Poppins-Bold', fontSize: 32}}>Hello</Text>
       <Text style={[globalStyles.text, {fontFamily: fonts.Bold}]}>World</Text>
       {/*  */}
-      <Text style={{fontFamily: fonts.Bold, fontSize: 32}}>Header 1</Text>
-      <Text style={{fontFamily: fonts.Bold, fontSize: 30}}>Header 2</Text>
-      <Text style={{fontFamily: fonts.Bold, fontSize: 28}}>Header 3</Text>
-      <Text style={{fontFamily: fonts.Bold, fontSize: 26}}>Header 4</Text>
-      <Text style={{fontFamily: fonts.Bold, fontSize: 24}}>Header 5</Text>
-      <Text style={{fontFamily: fonts.Bold, fontSize: 22}}>Header 6</Text>
-      <Text style={{fontFamily: fonts.Bold, fontSize: 20}}>Header 7</Text>
-      <Text style={{fontFamily: fonts.Bold, fontSize: 20}}>Body 1</Text>
-      <Text style={{fontFamily: fonts.Bold, fontSize: 18}}>Body 2</Text>
-      <Text style={{fontFamily: fonts.Bold, fontSize: 16}}>Caption</Text>
-      <Text style={{fontFamily: fonts.Bold, fontSize: 14}}>Label</Text>
+      <Text
+        style={{fontFamily: fonts.Bold, fontSize: 32, color: colors.dark.d500}}>
+        Header 1
+      </Text>
+      <Text
+        style={{fontFamily: fonts.Bold, fontSize: 30, color: colors.dark.d500}}>
+        Header 2
+      </Text>
+      <Text
+        style={{fontFamily: fonts.Bold, fontSize: 28, color: colors.dark.d500}}>
+        Header 3
+      </Text>
+      <Text
+        style={{fontFamily: fonts.Bold, fontSize: 26, color: colors.dark.d500}}>
+        Header 4
+      </Text>
+      <Text
+        style={{fontFamily: fonts.Bold, fontSize: 24, color: colors.dark.d500}}>
+        Header 5
+      </Text>
+      <Text
+        style={{fontFamily: fonts.Bold, fontSize: 22, color: colors.dark.d500}}>
+        Header 6
+      </Text>
+      <Text
+        style={{fontFamily: fonts.Bold, fontSize: 20, color: colors.dark.d500}}>
+        Header 7
+      </Text>
+      <Text
+        style={{fontFamily: fonts.Bold, fontSize: 20, color: colors.dark.d500}}>
+        Body 1
+      </Text>
+      <Text
+        style={{fontFamily: fonts.Bold, fontSize: 18, color: colors.dark.d500}}>
+        Body 2
+      </Text>
+      <Text
+        style={{fontFamily: fonts.Bold, fontSize: 16, color: colors.dark.d500}}>
+        Caption
+      </Text>
+      <Text
+        style={{fontFamily: fonts.Bold, fontSize: 14, color: colors.dark.d500}}>
+        Label
+      </Text>
       {/*  */}
-      <Text style={{fontFamily: fonts.Regular, fontSize: 32}}>Header 1</Text>
-      <Text style={{fontFamily: fonts.Regular, fontSize: 30}}>Header 2</Text>
-      <Text style={{fontFamily: fonts.Regular, fontSize: 28}}>Header 3</Text>
-      <Text style={{fontFamily: fonts.Regular, fontSize: 26}}>Header 4</Text>
-      <Text style={{fontFamily: fonts.Regular, fontSize: 24}}>Header 5</Text>
-      <Text style={{fontFamily: fonts.Regular, fontSize: 22}}>Header 6</Text>
-      <Text style={{fontFamily: fonts.Regular, fontSize: 20}}>Header 7</Text>
-      <Text style={{fontFamily: fonts.Regular, fontSize: 20}}>Body 1</Text>
-      <Text style={{fontFamily: fonts.Regular, fontSize: 18}}>Body 2</Text>
-      <Text style={{fontFamily: fonts.Regular, fontSize: 16}}>Caption</Text>
-      <Text style={{fontFamily: fonts.Regular, fontSize: 14}}>Label</Text>
+      <Text
+        style={{
+          fontFamily: fonts.Regular,
+          fontSize: 32,
+          color: colors.dark.d500,
+        }}>
+        Header 1
+      </Text>
+      <Text
+        style={{
+          fontFamily: fonts.Regular,
+          fontSize: 30,
+          color: colors.dark.d500,
+        }}>
+        Header 2
+      </Text>
+      <Text
+        style={{
+          fontFamily: fonts.Regular,
+          fontSize: 28,
+          color: colors.dark.d500,
+        }}>
+        Header 3
+      </Text>
+      <Text
+        style={{
+          fontFamily: fonts.Regular,
+          fontSize: 26,
+          color: colors.dark.d500,
+        }}>
+        Header 4
+      </Text>
+      <Text
+        style={{
+          fontFamily: fonts.Regular,
+          fontSize: 24,
+          color: colors.dark.d500,
+        }}>
+        Header 5
+      </Text>
+      <Text
+        style={{
+          fontFamily: fonts.Regular,
+          fontSize: 22,
+          color: colors.dark.d500,
+        }}>
+        Header 6
+      </Text>
+      <Text
+        style={{
+          fontFamily: fonts.Regular,
+          fontSize: 20,
+          color: colors.dark.d500,
+        }}>
+        Header 7
+      </Text>
+      <Text
+        style={{
+          fontFamily: fonts.Regular,
+          fontSize: 20,
+          color: colors.dark.d500,
+        }}>
+        Body 1
+      </Text>
+      <Text
+        style={{
+          fontFamily: fonts.Regular,
+          fontSize: 18,
+          color: colors.dark.d500,
+        }}>
+        Body 2
+      </Text>
+      <Text
+        style={{
+          fontFamily: fonts.Regular,
+          fontSize: 16,
+          color: colors.dark.d500,
+        }}>
+        Caption
+      </Text>
+      <Text
+        style={{
+          fontFamily: fonts.Regular,
+          fontSize: 14,
+          color: colors.dark.d500,
+        }}>
+        Label
+      </Text>
     </ScrollView>
   );
 };
