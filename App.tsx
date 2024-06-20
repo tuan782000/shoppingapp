6e061045639cd98606db0d05ec4f325623c1b7c3
bsdaoquang <bsdaoquang@gmail.com>
2024-06-20 15:27:24 +0700

    login with google for ios 2

--- App.tsx
+++ App.tsx
@@ -10,6 +10,8 @@ import {GoogleSignin} from '@react-native-google-signin/google-signin';
 GoogleSignin.configure({
   webClientId:
     '628813047631-qr5ioh2b13k2934rf717dkd172365a6v.apps.googleusercontent.com',
+  iosClientId:
+    '628813047631-ulnumkneilnhlraua2feop5g8ieda1si.apps.googleusercontent.com',
 });
 
 const App = () => {
